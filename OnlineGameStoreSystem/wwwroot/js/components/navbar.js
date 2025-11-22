16a3091327517667d65b5b6a63f1d6aa50f5912b
WONj46 <justinwong1125@gmail.com>
2025-11-22 21:42:07 +0800

    CRUD

--- OnlineGameStoreSystem/wwwroot/js/components/navbar.js
+++ OnlineGameStoreSystem/wwwroot/js/components/navbar.js
@@ -4,3 +4,10 @@ const searchBar = document.querySelector('.search-bar-container');
 searchBtn.addEventListener('click', () => {
     searchBar.classList.toggle('active');
 });
+function closeAlert() {
+    // 找到最外层的容器并隐藏它
+    var alertContainer = document.getElementById("temp-alert-container");
+    if (alertContainer) {
+        alertContainer.style.display = 'none';
+    }
+}
